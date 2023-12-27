7d48ffc6764b5edd1955199092271a8bae8b2065
Mario <max_cable@hotmail.com>
2023-12-27 14:47:02 -0600

    ADD GitHub provider

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,12 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
 import Google from "@auth/sveltekit/providers/google"
-import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "$env/static/private"
+import GitHub from "@auth/sveltekit/providers/github"
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
 
 export const handle = SvelteKitAuth({
-  providers: [Google({ clientId: GOOGLE_CLIENT_ID, clientSecret: GOOGLE_CLIENT_SECRET })],
+  providers: [
+    Google({ clientId: GOOGLE_CLIENT_ID, clientSecret: GOOGLE_CLIENT_SECRET }),
+    GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })
+  ],
+  pages: {}
 })
