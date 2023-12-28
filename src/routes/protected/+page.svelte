e7912af576fc343cc88c3b1d5f27adcc809ba611
Mario <31197372+Max131@users.noreply.github.com>
2023-12-28 14:08:27 -0600

    ADD login info

--- src/routes/protected/+page.svelte
+++ src/routes/protected/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import { page } from "$app/stores"
-  console.log($page.data.session)
 </script>
 
 {#if $page.data.session}
@@ -8,6 +7,15 @@
   <p>
     This is a protected content. You can access this content because you are
     signed in.
+  </p>
+  {#if $page.data.session.user?.image}
+    <p>
+      <img src={$page.data.session.user.image} alt="" width="96" height="96" />
+    </p>
+  {/if}
+  <p>
+    Signed in as: <strong>{$page.data.session.user?.name}</strong><br />
+    {$page.data.session.user?.email ?? $page.data.session.user?.name}
   </p>
   <p>Session expiry: {$page.data.session?.expires}</p>
 {:else}
