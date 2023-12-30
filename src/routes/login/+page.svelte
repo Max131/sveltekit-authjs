e50dc6e84488ee91261b5921dfd56f9a523f8294
Mario <max_cable@hotmail.com>
2023-12-29 23:17:42 -0600

    FIX signin function for multiple providers

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,59 +1,30 @@
 <script>
-  import { signIn, signOut } from "@auth/sveltekit/client"
   import { page } from "$app/stores"
+  import { signIn, signOut } from "@auth/sveltekit/client"
   import Helmet from "$lib/Helmet.svelte"
   import Google from "$lib/assets/Google.svelte"
   import Github from "$lib/assets/Github.svelte"
-  // let email = ""
-  // const handleEmailSignIn = () => {
-  //   signIn("email", { email, callbackUrl: "/protected" })
-  // }
 
-  const handleGoogleSignIn = () => {
-    signIn("google", { callbackUrl: "/protected" })
-  }
-
-  const handleGithubSignIn = () => {
-    signIn("github", { callbackUrl: "/protected" })
-  }
-
-  const handleSignOut = () => {
-    signOut()
+  const handleSignIn = (event) => {
+    const {
+      target: { dataset },
+    } = event
+    signIn(dataset.provider, { callbackUrl: "/protected" })
   }
 </script>
 
 <Helmet title="Login" />
 {#if !$page.data.session}
-  <!-- <img src={logo} alt="auth.js logo" class="logo" /> -->
   <h1 class="welcome">Welcome</h1>
   <p class="text">Log in to continue to SvelteKit Auth Example</p>
 
-  <!-- <form on:submit={handleEmailSignIn}>
-<input type="email" placeholder="example@gmail.com" bind:value={email} />
-<button>Continue</button>
-</form> -->
-
-  <!-- <div class="divider-container">
-<div />
-<span>OR</span>
-<div />
-</div> -->
-
-  <button on:click={handleGoogleSignIn}>
+  <button data-provider="google" on:click={handleSignIn}>
     <Google />
     <span> Continue with Google </span>
   </button>
 
-  <button on:click={handleGithubSignIn}>
+  <button data-provider="github" on:click={handleSignIn}>
     <Github />
     <span> Continue with Github </span>
   </button>
-{/if}
-
-{#if $page.data.session}
-  <div class="signout-container">
-    <!-- <img src={logo} alt="auth.js logo" class="logo" /> -->
-
-    <button on:click={handleSignOut}>Sign out</button>
-  </div>
 {/if}
