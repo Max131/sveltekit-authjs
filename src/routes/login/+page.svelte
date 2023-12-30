4ed2a43440332771598cec48f3d936d1bfa136db
Mario <max_cable@hotmail.com>
2023-12-30 12:02:37 -0600

    ADD loading state

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -5,27 +5,33 @@
   import Google from "$lib/assets/Google.svelte"
   import Github from "$lib/assets/Github.svelte"
 
+  let loading = false
+
   const handleSignInGoogle = () => {
+    loading = true
     signIn("google", { callbackUrl: "/protected" })
   }
 
   const handleSignInGithub = () => {
+    loading = true
     signIn("github", { callbackUrl: "/protected" })
   }
 </script>
 
 <Helmet title="Login" />
-{#if !$page.data.session}
-  <h1 class="welcome">Welcome</h1>
-  <p class="text">Log in to continue to SvelteKit Auth Example</p>
+<article aria-busy={loading}>
+  {#if !$page.data.session}
+    <h1 class="welcome">Welcome</h1>
+    <p class="text">Log in to continue to SvelteKit Auth Example</p>
 
-  <button on:click={handleSignInGoogle}>
-    <Google />
-    <span> Continue with Google </span>
-  </button>
+    <button disabled={loading} on:click={handleSignInGoogle}>
+      <Google />
+      <span> Continue with Google </span>
+    </button>
 
-  <button on:click={handleSignInGithub}>
-    <Github />
-    <span> Continue with Github </span>
-  </button>
-{/if}
+    <button disabled={loading} on:click={handleSignInGithub}>
+      <Github />
+      <span> Continue with Github </span>
+    </button>
+  {/if}
+</article>
