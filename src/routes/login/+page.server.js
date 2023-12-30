4a7457ec99cb57bc7cd3fa2a96bc42f58557717a
Mario <max_cable@hotmail.com>
2023-12-29 23:17:50 -0600

    FIX redirect on session

--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -1,5 +1,10 @@
+import { redirect } from "@sveltejs/kit";
+
 export const load = async (event) => {
   const session = await event.locals.getSession();
+  if (session) {
+    redirect(307, "/")
+  }
 
   return {
     session
