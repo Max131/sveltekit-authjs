4c76589d158fca8152d9227e8ee295fd49cc034f
Mario <31197372+Max131@users.noreply.github.com>
2024-03-03 22:43:46 -0600

    add get S3 bucket files

--- src/routes/protected/+page.server.js
+++ src/routes/protected/+page.server.js
@@ -4,7 +4,8 @@ import {
   ListObjectsV2Command,
   GetObjectCommand
 } from '@aws-sdk/client-s3'
-// import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
 
 const BUCKET = "kitcam"
 const S3 = new S3Client({
@@ -22,5 +23,15 @@ export const load = async () => {
       Bucket: BUCKET
     })
   )
-  return { files }
+
+  const signedURLS = await Promise.all(files.map(async (file) => {
+    const command = new GetObjectCommand({ Bucket: BUCKET, Key: file.Key })
+    const url = await getSignedUrl(S3, command, { expiresIn: 3600 })
+    return {
+      ...file,
+      url
+    }
+  }))
+
+  return { files: signedURLS }
 }
